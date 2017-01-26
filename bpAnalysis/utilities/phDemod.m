ab3907cb0744c148a1d5a863b37fd0162b35519d
Fitz Sturgill <fitzsturgill@gmail.com>
2017-01-26 14:05:21 -0500

    updated phDemod in which filtering occurs prior to quadrature demodulation

--- bpAnalysis/utilities/phDemod.m
+++ bpAnalysis/utilities/phDemod.m
@@ -7,7 +7,10 @@ function demod = phDemod(rawData, refData, sampleRate, modRate, lowCutoff)
     if nargin < 5
         lowCutoff = []; 
     end
-        
+    
+
+    
+    
 %     if nargin < 5
 %         tBaseline = []; % if empty, normalize (zscore) by entire range
 %     end
@@ -46,37 +49,44 @@ function demod = phDemod(rawData, refData, sampleRate, modRate, lowCutoff)
 
     processedData_0 = rawData .* refData;
     processedData_90 = rawData .* refData90;
-
-    demodData = (processedData_0 .^2 + processedData_90 .^2) .^(1/2); % quadrature decoding
-
+    %% try filtering first
+    % note-   5 pole Butterworth filter in Matlab used in Frohlich and McCormick  
      % Create butterworth filter
     lowCutoff = lowCutoff/sampleRate * 2; % multiply by 2 to convert to rad/sample- see butter documentation
     % for a cutoff freq of 300Hz and sample rate of 1000Hz, cutoff
-    % corresponds to 0.6pi rad/sample    300/1000 * 2 = 0.6
-
-    % note-   5 pole Butterworth filter in Matlab used in Frohlich and McCormick  
+    % corresponds to 0.6pi rad/sample    300/1000 * 2 = 0.6    
     [b, a] = butter(5, lowCutoff, 'low');   
     pad = 1;
     if pad
 %         paddedData = fliplr(demodData(1:sampleRate, 1)); % pad with 1s of reflected data
 %         paddedData = demodData(randperm(sampleRate), 1); % pad with 1s of randomized data (should still contain DC trend)
-        paddedData = demodData(1:sampleRate, 1); % AGV sez: pad with 1s of data, should be in phase as period should be an integer factor of 1 second
+        paddedData_0 = processedData_0(1:sampleRate, 1); % AGV sez: pad with 1s of data, should be in phase as period should be an integer factor of 1 second
+        paddedData_90 = processedData_90(1:sampleRate, 1); % AGV sez: pad with 1s of data, should be in phase as period should be an integer factor of 1 second        
         % HOWEVER- an additional problem is that there is a hardware onset
         % transient when the LED turns on
-        demodDataFilt = filtfilt(b,a,[paddedData; demodData]);
-        demod = demodDataFilt(length(paddedData) + 1: end, 1);
+        demodDataFilt_0 = filtfilt(b,a,[paddedData_0; processedData_0]);
+        demodDataFilt_90 = filtfilt(b,a,[paddedData_90; processedData_90]);        
+        demod_0 = demodDataFilt_0(length(paddedData_0) + 1:end, 1);
+        demod_90 = demodDataFilt_90(length(paddedData_90)+1:end, 1);        
     else
-        demod = filtfilt(b, a, demodData);
+%         demod_0 = filtfilt(b, a, demodData_0);
     end
+    
+    
+    
+    demod = (demod_0 .^2 + demod_90.^2) .^(1/2); % quadrature decoding
+
+
+
+
     
     % correct for amplitude of reference 
     
     % Vsig = Vsig*Vref/2 + Vsig*Vref/2 * Cos(2*Fmod * time)
     % you filter out the second term
     % multiply by two and divide by Vref to get Vsig
-    
+
     modAmp = calcSinusoidAmp(refData);
     demod = demod * 2 / modAmp;
 %     fig = ensureFigure('test', 1);
 %     plot(demodDataFilt);
-
