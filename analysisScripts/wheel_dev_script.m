a72df899024313461bd2e1f85ee212f8a6f6da88
Fitz Sturgill <fitzsturgill@gmail.com>
2017-05-08 18:51:35 -0400

    dev

---
+++ analysisScripts/wheel_dev_script.m
@@ -0,0 +1,34 @@
+%%
+ rawTimes = sessions(1).SessionData.RawEvents.Trial{1}.Events.Port3In;
+ wheelTimes = rawTimes(rawTimes < 30);
+ wheelY = ones(size(wheelTimes));
+ wheelTimes = [0 wheelTimes 30];
+ wheelY = cumsum([0 wheelY 0]);
+ 
+ [wheelY_new, wheelTimes_new] = resample(wheelY, wheelTimes, 20, 'linear');
+ 
+ ensureFigure('test', 1);
+ plot(wheelTimes, wheelY); hold on;
+ plot(wheelTimes_new, wheelY_new);
+ 
+ 
+ %%
+D = 12.7; % diameter of wheel in cm
+pr = 200; % pulses/rotation 
+dpp = pi * 12.7 / 200;
+rawTimes = sessions(1).SessionData.RawEvents.Trial{2}.Events.Port3In;
+wheelTimes = rawTimes(rawTimes < 30);
+edges = 0:1/20:30;
+wheel_X = cumsum(histcounts(rawTimes, edges) * dpp);
+% wheel_X = smooth(cumsum(histcounts(rawTimes, edges)), 20);  
+wheel_t = 0:1/20:30-1/20;
+  
+ensureFigure('test', 1);
+subplot(2,1,1);
+plot(wheelTimes, cumsum(ones(size(wheelTimes)) * dpp), 'b'); hold on;
+plot(wheel_t, wheel_X, 'g');
+
+subplot(2,1,2);
+plot(wheel_t(1:end-1), diff(wheel_X), 'b'); hold on
+plot(wheel_t, gradient(wheel_X), 'g');
+ 
--- bpAnalysis/Photometry/processTrialAnalysis_Wheel.m
+++ bpAnalysis/Photometry/processTrialAnalysis_Wheel.m
@@ -1,4 +1,4 @@
-function Photometry = processTrialAnalysis_Wheel(sessions, Fs, Ns, varargin)
+function Photometry = processTrialAnalysis_Wheel(sessions, varargin)
 
 % Fs - sample rate
 % Ns - number of samples/trial  (currently a scalar value, uniformOutput =
@@ -10,19 +10,59 @@ function Photometry = processTrialAnalysis_Wheel(sessions, Fs, Ns, varargin)
     defaults = {...
         'dX', 5;... %unit length in cm traveled for each advance of the rotary encoder, (currently a weakly founded guess)
         'uniformOutput', 1;...            % not currently implemented, idea is to set to 0 if acqs are going to be variable in length (store data in cell array)
-        'startField', 'PreCsRecording'
+        'startField', 'Start';...
+        'zeroField', '';... % if empty, startField defines zero
+        'Fs', 20;...
+        'duration', 30;...
+        'dataField', 'Port3In';...
         };
-    [s, ~] = parse_args(defaults, varargin{:}); % combine default and passed (via varargin) parameter settings
-    s.Fs = Fs;    
     
+    [s, ~] = parse_args(defaults, varargin{:}); % combine default and passed (via varargin) parameter settings 
+    if isempty(s.zeroField)
+        s.zeroField = s.startField;
+    end
     % find total number of trials across selected sessions and size of
     % nidaq data
     scounter = zeros(size(sessions));
     for i = 1:length(sessions)
         scounter(i) = sessions(i).SessionData.nTrials;
     end
-    totalTrials = sum(scounter);    
-
-    if s.uniformOutput 
-
-        xData = linspace(-zeroTime, ((newSamples - 1) / sampleRate) - zeroTime, newSamples);    
+    totalTrials = sum(scounter);        
+    if s.uniformOutput        
+        zeroTime = sessions(1).SessionData.RawEvents.Trial{1}.States.(s.zeroField)(1) - ...
+            sessions(1).SessionData.RawEvents.Trial{1}.States.(s.startField)(1);
+        xData = linspace(-zeroTime, -zeroTime + s.duration, round(s.duration/s.Fs));
+    else
+        error(' uniformOutput == 0 mode not yet implemented ');
+    end
+    
+    Wheel = struct(...
+        'data', [],...
+        'settings', s,...
+        'startTime', NaN(totalTrials, 1),...
+        'xData', xData...
+        );
+    if s.uniformOutput
+        data = struct(...
+            'V', NaN(totalTrials, length(xData)),...
+            );
+    else
+        data = struct(...
+            'V', {}...
+            );        
+    end
+    
+    tcounter = 1;    
+    for si = 1:length(sessions);
+        SessionData = sessions(si).SessionData;
+        startTimes = cellfun(@(x) x.States.(s.startField)(1), sessions(si).SessionData.RawEvents.Trial); % take the beginning time stamp for the startField-specified Bpod state
+        nTrials = SessionData.nTrials;
+        allData = NaN(nTrials, newSamples);   
+            for trial = 1:nTrials
+                
+                
+            end
+        Wheel.startTime(tcounter:tcounter+nTrials - 1) = startTimes';
+        tcounter = tcounter + nTrials;            
+    end
+                
