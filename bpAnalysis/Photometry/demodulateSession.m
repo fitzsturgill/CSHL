afa1e1350dfbeecb28c802519372c3a2db90330b
Fitz Sturgill <fitzsturgill@gmail.com>
2017-06-20 10:42:19 -0400

    finished and debugged phDemod_v2 and made demodulateSession handle old and new style demodulation

--- bpAnalysis/Photometry/demodulateSession.m
+++ bpAnalysis/Photometry/demodulateSession.m
@@ -34,11 +34,12 @@ SessionData.demod = cell(SessionData.nTrials, 2);
 
 
 for trial = 1:SessionData.nTrials
-    for fCh = s.channels             
+    for counter = 1:length(s.channels)
+        fCh = s.channels(counter);
 %% Determine demodulation mode:    
 %% case 1: reference data saved (prior to 6/2017)
         if trial == 1
-            if ~isstruct(sessions.SessionData.NidaqData{1,2}) % figure this out from the first channel
+            if ~isstruct(SessionData.NidaqData{1,2}) % figure this out from the first channel
                 if isfield(SessionData.TrialSettings(1,1), 'nidaq') % new style data acq parameters in nidaq field 
                     modF = SessionData.TrialSettings(1,1).nidaq.(['LED' num2str(fCh) '_f']); % this could change (I should store modF in settings not trialsettings)        
                 else % old style
@@ -61,16 +62,17 @@ for trial = 1:SessionData.nTrials
             continue
         end
         nSamples = size(rawData, 1);
+        if s.ACfilter(counter) % whether or not to apply AC filtering for this channel
+            rawData = filtfilt(b, a, rawData);        
+        end
         switch demodMode
             case 1
                 refData = SessionData.NidaqData{trial,2}(1:nSamples,fCh);
+                finalData = phDemod(rawData, refData, sampleRate, modF, lowpass); % lowpass corner freq                
             case 2
                 refData = SessionData.NidaqData{trial,2};
+                finalData = phDemod_v2(rawData, refData, s.refChannels(counter), sampleRate); 
         end
-        if ACfilter
-            rawData = filtfilt(b, a, rawData);        
-        end 
-        finalData = phDemod(rawData, refData, sampleRate, modF, lowpass); % lowpass corner freq
         SessionData.demod{trial,fCh} = finalData;
     end
 end        
