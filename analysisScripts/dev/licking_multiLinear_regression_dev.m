7663221c9a6dec46bad5e54c306be84725420395
Fitz Sturgill <fitzsturgill@gmail.com>
2018-03-27 11:48:41 -0400

    in progress

--- analysisScripts/dev/licking_multiLinear_regression_dev.m
+++ analysisScripts/dev/licking_multiLinear_regression_dev.m
@@ -1,44 +1,54 @@
 % multilinear regression with licking
 
-load('Z:\SummaryAnalyses\LickNoLick_odor_v2_BaselineTrialByTrial\DC_37\TE.mat');
+% load('Z:\SummaryAnalyses\LickNoLick_odor_v2_BaselineTrialByTrial\DC_37\TE.mat');
+load('Z:\SummaryAnalyses\LickNoLick_odor_v2\DC_35\TE.mat');
 LNL_conditions;
-
+%%
+TE.licksTS = bpEventToTimeSeries(TE, 'Port1In', 'duration', 11);
 
 %%
 % Initial Conditions
 Fs = 20; % sample rate
 channel = 1;
+Kw = [-2 2]; % Kernel/weight offset window
+
+
+% generate weight time offset indices
+Kw2 = Kw * Fs;
+Kwix = Kw2(1):Kw2(2);
+margins = [sum(Kwix < 0) sum(Kwix) > 0]; % to avoid edge effects due to using circ-shifted predictor variables
 
 % Predictor
 T = TE.Photometry.data(channel).ZS';
-
-% T = ([1 1 1 1; 2 2 2 2; 3 3 3 3]); 
+T = T(margins(1) + 1 : end - margins(2), :);
+truncSize = size(T);
+% T = ([1 1 1 1; 2 2 2 2; 3 3 3 3]);
 T = T(:);
 T = zscore(T);
 
-% Kernel/weight offset window
-Kw = [-2 2];
-Kw = Kw * Fs;
-% weight time offset indices
-Kwix = Kw(1):Kw(2);
-
 % Design matrix
-dm = TE.licksTS.data' > 0;
-% dm = ([0:100; 50:150; 100:200])'; 
-DM = zeros(size(dm,1), size(dm,2), length(Kwix));
+dm = TE.licksTS.data' > 0; % get rid of spurious fast bursts of licks
+% dm = ([0:100; 50:150; 100:200])';
+DM = zeros(truncSize(1), truncSize(2), length(Kwix));
 
 % shift matrices
 for counter = 1:length(Kwix)
-    DM(:,:,counter) = circshift(dm, Kwix(counter), 1);
+    shifted = circshift(dm, Kwix(counter), 1);
+    DM(:,:,counter) = shifted(margins(1) + 1 : end - margins(2), :);
 end
 
 % reshape
-DM = reshape(DM, numel(dm), length(Kwix));
+DM = reshape(DM, prod(truncSize), length(Kwix));
 
 % regress
-W = regress(T, DM);
+[w wconf R] = regress(T, DM);
 
-%%
-ensureFigure('test', 1); plot(W);
-
-disp('good progress but see commit, issue of predictors circ shift edge effects');
+% xData for plotting weight kernel
+xData = linspace(Kw(1), Kw(2), length(w));
+%
+ensureFigure('test', 1); boundedline(xData', w, wconf - w);
+ensureFigure('test', 1); plot(xData', w, 'b'); hold on; plot(xData', wconf,'c')
+R = reshape(R, truncSize(1), truncSize(2));
+R = R';
+%
+ensureFigure('test2', 1); imagesc(R, [-2 2]);
