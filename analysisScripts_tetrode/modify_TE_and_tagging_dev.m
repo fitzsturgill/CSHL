1e42bffd4d3d51086072af9488d6be621738fde9
Fitz Sturgill <fitzsturgill@gmail.com>
2018-08-09 11:32:12 -0400

    in progress

--- analysisScripts_tetrode/modify_TE_and_tagging_dev.m
+++ analysisScripts_tetrode/modify_TE_and_tagging_dev.m
@@ -15,9 +15,13 @@ thesecells = findcell('rat', rat, 'session', session);
 loadcb; % % loads ANALYSES, CELLIDLIST, PREFERENCES, and TheMatrix
 pref = getcbpref; % torben's new way
 
-cellid = thesecells{4};
+cellid = thesecells{5};
+[ratname,session,tetrode,unit] = cellid2tags(cellid);
 TE = loadcb(cellid, 'TrialEvents');
 SP = loadcb(cellid,'EVENTSPIKES');
+eventIx = find(strcmp(SP.events(:,1), 'Us_start'));
+
+stimes = SP.event_stimes{eventIx};
 
 
 %% add number of outcome Licks occuring between 0.1 and 1s following outcome
@@ -28,18 +32,24 @@ TE.csLicks_rate = TE.csLicks.rate;
 TE.rewardReceived = TE.usLicks.count >= 2; % get rid of trials where mouse fails to lick for reward
 save(fullfile(pref.datapath, rat, session, pref.TrialEvents_fname), 'TE'); 
 
-
 % make rasters and PSTHs
 time = window(1) - winMargin:dt:window(2) + winMargin;
 binraster = stimes2binraster(stimes,time,dt);
+
 %%
 ensureFigure('test1', 1); viewcell2b(cellid,'TriggerName','Us_start','SortEvent','usLicks_rate','eventtype','behav','ShowEvents',{'Us_start'},...
    'Partitions','#trialType: {1 4 7} & rewardReceived','window',[-7 4], 'dt', 0.01, 'sigma', 0.02, 'PSTHstd', 'on', 'isadaptive', true);
 %%
-ensureFigure('test2', 1); viewcell2b(cellid,'TriggerName','Us_start','SortEvent','csLicks_rate','eventtype','behav','ShowEvents',{'Us_start'},...
-   'Partitions','#trialType: {1 4 7}','window',[-7 4], 'dt', 0.02, 'sigma', 0.02, 'PSTHstd', 'on', 'isadaptive', true);
+ensureFigure('test_spikes', 1);  viewcell2b(cellid,'TriggerName','Us_start','SortEvent','csLicks_rate','eventtype','behav','ShowEvents',{'Us_start'},...
+   'Partitions','#trialType: {1 4 7} & rewardReceived','window',[-3 2], 'dt', 0.02, 'sigma', 0.02, 'PSTHstd', 'on', 'isadaptive', true);
+
+ensureFigure('test_lick', 1); viewlick({rat, session}, 'TriggerName', 'Us_start', 'SortEvent', 'csLicks_rate', 'eventtype', 'behav', 'ShowEvents', {'Us_start'},...
+    'Partitions', '#trialType: {1 4 7} & rewardReceived','window',[-3 2], 'dt', 0.02, 'sigma', 0.02, 'PSTHstd', 'on', 'isadaptive', true, 'LickInField', 'Port1In');
+
 %%
 ensureFigure('test3', 1); viewcell2b(cellid,'TriggerName','Us_start','SortEvent','csLicks_rate','eventtype','behav','ShowEvents',{'Us_start'},...
    'Partitions','all','window',[-7 4], 'dt', 0.02, 'sigma', 0.02, 'PSTHstd', 'on', 'isadaptive', true);
+
+
 
 
