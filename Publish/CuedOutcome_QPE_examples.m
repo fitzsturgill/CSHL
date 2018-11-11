fc06dafc0a9f42fc64e876341730c405a45f2930
FITZ-DESKTOP\Fitz <fitzsturgill@gmail.com>
2018-11-11 16:35:34 -0500

    in progress

--- Publish/CuedOutcome_QPE_examples.m
+++ Publish/CuedOutcome_QPE_examples.m
@@ -1,7 +1,17 @@
 % CuedOutcome_QPE_examples
+DB = dbLoadExperiment('cuedOutcome');
+savepath = fullfile(DB.path, ['pooled' filesep 'figure']);
+ensureDirectory(savepath);
+saveOn = 1;
 
-%% Snippet to make lick histogram for graded value task from DC_35: (or DC_26?)
 
+% photometryField = 'Photometry';
+% fdField = 'ZS';
+
+
+%% Lick histogram for graded value task
+animal = 'ChAT_39';
+success = dbLoadAnimal(DB, animal); % load TE and trial lookups
  % cue types
     varargin = {'trialNumbering', 'consecutive',...
         'window', [-6 4], 'zeroField', 'Us', 'startField', 'PreCsRecording', 'endField', 'PostUsRecording',...
@@ -10,16 +20,14 @@
     ensureFigure('Lick_Hist_CCN', 1); axes;
 
     [ha, hl] = plotEventAverageFromTE(TE, trialsByType([1 4 7]), 'Port1In', varargin{:});
-    set(hl, 'LineWidth', 2);
-    legend(hl, {'\color{blue} high value', '\color{red} low value', '\color{green} uncued'}, 'Location', 'northwest', 'FontSize', 16, 'Interpreter', 'tex'); legend('boxoff');
     set(gca, 'XLim', [-6 4], 'YLim', [-2 15]);
     addStimulusPatch(gca, [-3 -2 10], '', [0.7 0.7 0.7]) 
     addStimulusPatch(gca, [-0.1 0.1], '');
+    legend(hl, {'\color{blue} high value', '\color{red} low value', '\color{green} uncued'}, 'FontSize', 6, 'Interpreter', 'tex', 'Position', [0.25 0.6 0.3 0.4]); legend('boxoff');    
     ylabel('Licks (Hz)'); xlabel('Time from reinforcement (s)');
-    formatFigureTalk([4 3]);
-    if saveOn    
-        saveas(gcf, fullfile(savepath, 'Lick_Hist_CCN.fig'));
-        saveas(gcf, fullfile(savepath, 'Lick_Hist_CCN.jpg'));    
-        saveas(gcf, fullfile(savepath, 'Lick_Hist_CCN.emf'));
-        saveas(gcf, fullfile(savepath, 'Lick_Hist_CCN'), 'epsc');        
-    end
+    
+    formatFigurePublish('size', [2 1.1]);
+    if saveOn 
+        export_fig(fullfile(savepath, saveName), '-eps');
+    end    
+    
