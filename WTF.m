b155f51c2278528812d2f4cbb5b581dea7d0cdde
FITZ-DESKTOP\Fitz <fitzsturgill@gmail.com>
2018-12-22 19:53:56 -0500

    stuff

--- WTF.m
+++ WTF.m
@@ -1,41 +1,38 @@
-%% plot example reversals with weibull fits, changepoints
-nShow = 6;
-% good revs include [77 3 48 74];,   3 and 74 best examples
-% mediocre,  84 and 91 and 42 are mediocre ones
-% 4 is good but gradual
-toShow = find(goodReversals);
+basepath = 'C:\Users\Fitz\Documents\Data_Local\iChR2_test\';
 
-ordering = randperm(sum(goodReversals), nShow);
-toShow = toShow(ordering);
-toShow2 = 1:sum(goodReversals);
-toShow2 = toShow2(ordering);
+folders = dir(basepath);
+folders = folders(3:end);
 
-% nShow = 4;  toShow = [74 4 91 42];
-ensureFigure('reversals_pooled_Latency_examples', 1);
 
-for counter = 1:nShow   
-    thisRev = toShow(counter);
-    thisRev2 = toShow2(counter);
-    % weibull
-    subplot(nShow, 3, counter*3 - 2);
-%     plot(weibull(thisRev).toFit, 'g.'); hold on;
-%     plot(weibull(thisRev).object); legend off;
-    set(gca, 'XLim', [0 120]);
-    % changepoint
-    subplot(nShow, 3, counter*3 - 1); hold on;
-    scatter(1:length(cp_licks.cumsum{thisRev}), cp_licks.cumsum{thisRev}, 10, cp_licks.logitAll{thisRev}); colormap jet;
-    line(repmat(cp_licks.index(thisRev), 1, 2), get(gca, 'YLim')); set(gca, 'XLim', [0 120]);
-    textBox(sprintf('Logit=%.2f', cp_licks.logit(thisRev)));
-    % exponential
-    subplot(nShow, 3, counter*3); hold on;    
-%     plot(expFit.csPlus.licks_cs.toFit{thisRev2}, 'g.'); hold on;
-%     plot(expFit.csPlus.licks_cs.object{thisRev2}); legend off;
-    set(gca, 'XLim', [0 120]);
+for counter = 2:length(folders)
+%     try
+        probePSTH2('filepath', fullfile(basepath, folders(counter).name, filesep), 'avg_window', [-3 4]);
+%     catch ME
+        disp(fullfile(basepath, folders(counter).name, filesep));
+%     end
 end
-
+    
+%%
 return;
 %%
-    ensureFigure('test', 1);
-    scatter(1:length(cp_licks.cumsum{thisRev}), cp_licks.cumsum{thisRev}, 10, cp_licks.logitAll{thisRev}); colormap jet;
-    line(repmat(cp_licks.index(thisRev), 1, 2), get(gca, 'YLim')); set(gca, 'XLim', [0 120]);
-    textBox(sprintf('Logit=%.2f', cp_licks.logit(thisRev)));
+ensureFigure('test', 1); 
+
+offset = 0;
+for counter = 1:32
+    subplot(4,8,counter);
+    scatter(stimData(counter + offset).spikeTimes, stimData(counter + offset).spikeAmplitudes, '.');
+    textBox(['ch' num2str(counter + offset)]);
+end
+
+
+%%
+
+ensureFigure('test2', 1); 
+
+offset = 0;
+for counter = 1:32
+    subplot(16,2,counter);
+    plot(linspace(-1, 2, size(stimData(counter).snippet, 2)), mean(stimData(counter).snippet));
+    textBox(['ch' num2str(counter + offset)]);
+%     set(gca, 'XLim', [3.2 4.2]); set(gca, 'YLim', [-200 200]);
+end
