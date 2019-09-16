9dc3316f0fee3eecb9d8e99c732182ace3cec8d3
FITZ-DESKTOP\Fitz <fitzsturgill@gmail.com>
2019-09-15 20:25:22 -0400

    whateves

--- Publish/reversals_pooled/reversals_noPunish_poolReversals_changepoints.m
+++ Publish/reversals_pooled/reversals_noPunish_poolReversals_changepoints.m
@@ -848,12 +848,12 @@ comp = {'Lick_vs_ACh'; 'Lick_vs_Dop'; 'ACh_vs_Dop'};
 acq_p = zeros(3,1);
 ext_p = zeros(3,1);
 cp_stats = table(comp, acq_p, ext_p);
-cp_stats.acq_p(1) = signrank(all_cps(:,1), all_cps(:,2));
-cp_stats.acq_p(2) = signrank(all_cps(:,1), all_cps(:,3));
-cp_stats.acq_p(3) = signrank(all_cps(:,2), all_cps(:,3));
-cp_stats.ext_p(1) = signrank(all_cps(:,4), all_cps(:,5));
-cp_stats.ext_p(2) = signrank(all_cps(:,4), all_cps(:,6));
-cp_stats.ext_p(3) = signrank(all_cps(:,5), all_cps(:,6));
+cp_stats.acq_p(1) = signrank(all_cps(goodReversals,1), all_cps(goodReversals,2));
+cp_stats.acq_p(2) = signrank(all_cps(goodReversals,1), all_cps(goodReversals,3));
+cp_stats.acq_p(3) = signrank(all_cps(goodReversals,2), all_cps(goodReversals,3));
+cp_stats.ext_p(1) = signrank(all_cps(goodReversals,4), all_cps(goodReversals,5));
+cp_stats.ext_p(2) = signrank(all_cps(goodReversals,4), all_cps(goodReversals,6));
+cp_stats.ext_p(3) = signrank(all_cps(goodReversals,5), all_cps(goodReversals,6));
 
 %% despite paired measurements, weak correlations between detected changepoints
 savename = ['ChangePoint_correlations_scatter' '_' expType];
